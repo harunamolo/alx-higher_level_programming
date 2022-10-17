67bab4cc91b845d62b45c83a375d0102ab26ef13
harunamolo <107302226+harunamolo@users.noreply.github.com>
2022-10-17 23:01:43 +0300

    Add files via upload

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""define a Rectangle"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """init for Rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Rectangle width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Rectangle width setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Rectangle height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Rectangle height setter"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
