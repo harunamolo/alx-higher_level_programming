f2a27583d8bb702ed7a77bc4b62cce97958b0b9c
harunamolo <107302226+harunamolo@users.noreply.github.com>
2023-01-24 20:16:54 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sd "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
